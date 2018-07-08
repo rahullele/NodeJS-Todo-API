3823d7c8bad090dcd77df64d808c454810565739
rahullele <rlele@usc.edu>
2018-07-08 02:17:09 -0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express=require('express');
 var bodyParser=require('body-parser');
 
 
-
+const {ObjectID}=require('mongodb');
 var {mongoose}=require('./db/mongoose');
 var {Todo}=require('./models/todo');
 var {User}=require('./models/user');
@@ -17,18 +17,6 @@ app.post('/todos',(req,res)=>{
 var todo=new Todo({
   text:req.body.text
 });
-
-app.get('/todos',(req,res)=>{
-
-Todo.find().then((todos)=>{
-res.send({todos});
-},(e)=>{
-res.status(400).send(e);
-});
-});
-
-
-
 todo.save().then((doc)=>{
 
   res.send(doc);
@@ -36,6 +24,38 @@ todo.save().then((doc)=>{
   res.status(400).send(e);
 });
 });
+// app.get('/todos',(req,res)=>{
+//
+// Todo.find().then((todos)=>{
+// res.send({todos});
+// },(e)=>{
+// res.status(400).send(e);
+// });
+// });
+
+//GET /todos/1234324
+
+app.get('/todos/:id',(req,res)=>{
+  var id=req.params.id;
+  if(!ObjectID.isValid(id)){
+    console.log('ID not valid');
+    return res.status(404).send();
+  }
+
+
+  Todo.findById(id).then((todo)=>{
+
+    if(!todo){
+      return res.status(404).send();;
+    }
+    res.send({todo});  //identical to {todo:todo}
+  },(e)=>{
+    res.status(400).send(e);
+  }).catch((e)=>console.log(e));
+
+});
+
+
 
 
 app.listen(3000,()=>{
