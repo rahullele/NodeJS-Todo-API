acbf4ee727ec61c358230457042a9e37612edecd
rahullele <rlele@usc.edu>
2018-07-08 19:10:10 -0700

    Setup for app on heroku

--- server/server.js
+++ server/server.js
@@ -27,14 +27,14 @@ todo.save().then((doc)=>{
   res.status(400).send(e);
 });
 });
-// app.get('/todos',(req,res)=>{
-//
-// Todo.find().then((todos)=>{
-// res.send({todos});
-// },(e)=>{
-// res.status(400).send(e);
-// });
-// });
+app.get('/todos',(req,res)=>{
+
+Todo.find().then((todos)=>{
+res.send({todos});
+},(e)=>{
+res.status(400).send(e);
+});
+});
 
 //GET /todos/1234324
 
